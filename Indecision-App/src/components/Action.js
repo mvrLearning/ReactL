7faa0ded7a6f312b52c3cbd9a248e0b14c4e3344
Mareddy Venkateswara Reddy <mareddy.reddy@wipro.com>
2018-03-19 16:16:11 +0530

    Indecision App full

--- Indecision-App/src/components/Action.js
+++ Indecision-App/src/components/Action.js
@@ -2,7 +2,8 @@ import React from "react";
 const Action = (props) => {
     return (
         <div>
-            <button onClick={props.handlePick}
+            <button className="big-button"
+            onClick={props.handlePick}
                 disabled={!props.hasOptions}
             >What Should I do?</button>
         </div>
--- Indecision-App/src/components/Options.js
+++ Indecision-App/src/components/Options.js
@@ -2,12 +2,16 @@ import React from "react";
 import Option from './Option';
 const Options = (props) => (
     <div>
-
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please Add option to Continue</p>}
-        {props.options.map((option) => (<Option
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button className="button button--link"
+                onClick={props.handleDeleteOptions}>Remove All</button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please Add option to Continue</p>}
+        {props.options.map((option,index) => (<Option
             key={option}
             optionText={option}
+            count = {index + 1}
             handleDeleteOption={props.handleDeleteOption}
         />
         ))
